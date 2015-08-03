72a7070f1e991dd8d8a14989a36a0d8452043f3c
clearives <xianghui88998@163.com>
2015-08-03 09:44:17 +0800

    获取外部样式方法

--- html5/tool/getCurrentStyle.js
+++ html5/tool/getCurrentStyle.js
@@ -1,7 +1,9 @@
 /**
  * Created by Clearives on 2015/8/3.
  */
-// 参数node：将要获取其计算样式的元素节点
+/**
+ *  {node} 将获取样式的节点
+ */
 function getCurrentStyle(node) {
     var style = null;
     if(window.getComputedStyle) {
