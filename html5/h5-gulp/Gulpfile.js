fd589f3df6a81ab95f2d9d570191eda25a46dead
clearives <xianghui88998@163.com>
2015-08-06 14:04:24 +0800

    readme.md

--- html5/h5-gulp/Gulpfile.js
+++ html5/h5-gulp/Gulpfile.js
@@ -33,10 +33,6 @@ gulp.task('help',function () {
     console.log('	gulp build			文件打包');
     console.log('	gulp watch			文件监控打包');
     console.log('	gulp help			gulp参数说明');
-    console.log('	gulp server			测试server');
-    console.log('	gulp -p				生产环境（默认生产环境）');
-    console.log('	gulp -d				开发环境');
-    console.log('	gulp -m <module>		部分模块打包（默认全部打包）');
 });
 
 
---
+++ html5/h5-gulp/README.md
@@ -0,0 +1,7 @@
+###定制化项目自动化：
+
+	#####gulp build			文件打包
+	#####gulp watch			文件监控打包
+	#####gulp help			gulp参数说明
+
+
