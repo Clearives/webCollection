0aaca33d063f932752966e7e3da26ebfd99df42b
clearives <xianghui88998@163.com>
2015-10-26 14:02:28 +0800

    units arrHelper

--- html5/units/arrHelper.js
+++ html5/units/arrHelper.js
@@ -16,4 +16,13 @@ define(function(require, exports) {
         return false;
     };
 
+    /**
+     * 用Math.random()函数生成0~1之间的随机数与0.5比较
+     * @返回-1或1
+     * @returns {Number}
+     */
+    exports.randomsort = function randomsort() {
+        return Math.random()>.5 ? -1 : 1;
+    }
+
 });
